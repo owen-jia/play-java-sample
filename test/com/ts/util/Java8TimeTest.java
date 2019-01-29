8cc235ce47048029a96f0a72c4fe87decb05ac3b
贾文军 <jiawenjun@isesol.com>
2019-01-29 13:17:22 +0800

    java8time包测试用例编写

--- test/com/ts/util/Java8TimeTest.java
+++ test/com/ts/util/Java8TimeTest.java
@@ -2,7 +2,11 @@ package com.ts.util;
 
 import java.time.*;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
 import java.time.format.FormatStyle;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
+import java.util.Date;
 
 /**
  * @author: Owen Jia
@@ -15,10 +19,34 @@ public class Java8TimeTest {
         LocalDateTime dateTime = LocalDateTime.now(Clock.system(ZoneId.systemDefault()));
         LocalDateTime datetime2 =dateTime.minusDays(2);
 
+        printTest(dateTime);
+        printTest(datetime2);
+        printTest(datetime2.plusHours(3));
+        printTest(datetime2.minusWeeks(1));
+        printTest(datetime2.plus(1,ChronoUnit.MONTHS));
+
+        printTest(datetime2.compareTo(dateTime));
+        printTest(datetime2.withYear(2));
+        printTest(datetime2.isBefore(dateTime));
+
+        Duration duration = Duration.ofDays(5);
+        printTest(duration);
+        printTest(duration.plusHours(2).toMinutes());
     }
 
     public static void dateFormatTest(){
+        LocalDateTime dateTime = LocalDateTime.now(Clock.systemDefaultZone());
 
+        printTest(dateTime);
+        printTest(dateTime.format(DateTimeFormatter.BASIC_ISO_DATE));
+        printTest(dateTime.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
+        printTest(dateTime.format(DateTimeFormatter.ISO_WEEK_DATE));
+        printTest(dateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")));
+
+        DateTimeFormatterBuilder builder = new DateTimeFormatterBuilder();
+        builder.appendPattern("yyyy-MM-dd");
+        builder.parseStrict().toFormatter();
+        printTest(dateTime.format(builder.parseStrict().toFormatter()));
     }
 
     public static void dateStringTest(){
@@ -40,13 +68,35 @@ public class Java8TimeTest {
 
     public static void date2OldTest(){
 
+        LocalDateTime localDateTime = LocalDateTime.now();
+        localDateTime.minusHours(2);
+        printTest(localDateTime);
+        Date localDateTime2 = Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
+        printTest(localDateTime2.toString());
+
+        LocalDate localDate = LocalDate.now();
+        printTest(localDate);
+        Date localDate2 = Date.from(localDate.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant());
+        printTest(localDate2);
+
+        Date date = new Date();
+        printTest(date);
+        LocalDateTime date2 = LocalDateTime.ofInstant(date.toInstant(),ZoneId.systemDefault());
+        printTest(date2);
+
+        LocalTime localTime = LocalDateTime.ofInstant(new Date().toInstant(),ZoneId.systemDefault()).toLocalTime();
+        printTest(localTime);
     }
 
     public static void printTest(Object print){
-        System.out.println(printCount++ + " : " + print);
+        if(print instanceof LocalDateTime){
+            LocalDateTime localDateTime = (LocalDateTime) print;
+            System.out.println(printCount++ + " : " + localDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+        } else
+            System.out.println(printCount++ + " : " + print);
     }
 
     public static void main(String[] args){
-        Java8TimeTest.dateStringTest();
+        Java8TimeTest.date2OldTest();
     }
 }
