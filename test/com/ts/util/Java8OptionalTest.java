b85119ba56fc2da1bb18357cf7d2770ad9ba165c
贾文军 <jiawenjun@isesol.com>
2019-02-01 18:20:27 +0800

    optional测试用例思考

--- test/com/ts/util/Java8OptionalTest.java
+++ test/com/ts/util/Java8OptionalTest.java
@@ -19,7 +19,7 @@ public class Java8OptionalTest {
 
     ICar car = new WeiLaiCar();
 
-    public static void main(String[] args){
+    public static void unknowTest(){
         Java8OptionalTest test = null;
         Java8OptionalTest test2 = new Java8OptionalTest();
         test2.setCar(new WeiLaiCar());
@@ -41,12 +41,33 @@ public class Java8OptionalTest {
         pringTest(filterTest(test));
     }
 
-    public static void pringTest(Object print){
-        System.out.println(sum++ + " : " +print);
+    public static void mapTest(){
+
     }
 
-    public List<String> sayHello(){
-        return Optional.ofNullable(stringList).orElse(Collections.EMPTY_LIST);
+    public static void filterTest(){
+
+    }
+
+    public static void orElseTest(){
+
+    }
+
+    public static void equilsTest(){
+        Java8OptionalTest test2 = new Java8OptionalTest();
+        Java8OptionalTest test1 = test2;
+        Java8OptionalTest test3 = new Java8OptionalTest();
+        pringTest(Optional.of(test1).equals(test1));
+        pringTest(Optional.of(test1).equals(test2));
+        pringTest(Optional.of(test1).equals(test3));
+    }
+
+    public static void main(String[] args){
+        Java8OptionalTest.equilsTest();
+    }
+
+    public static void pringTest(Object print){
+        System.out.println(sum++ + " : " +print);
     }
 
     public static Integer printWheel(Java8OptionalTest test){
@@ -56,6 +77,7 @@ public class Java8OptionalTest {
     public static Boolean filterTest(Java8OptionalTest test){
         return Optional.ofNullable(test).filter(t -> t.getCar() != null && t.getCar().getWheelCount() > 0).isPresent();
     }
+
 
     public ICar getCar() {
         return car;
