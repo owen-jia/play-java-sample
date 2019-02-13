9aa984418f668bdeedee82e9a5b540b1e1994f32
贾文军 <jiawenjun@isesol.com>
2019-02-13 18:47:01 +0800

    script测试代码

---
+++ test/com/ts/util/java8/scripts/JavaScriptTest.java
@@ -0,0 +1,16 @@
+package com.ts.util.java8.scripts;
+
+import com.ts.util.PrintUtil;
+
+/**
+ * java8script 测试
+ * @author: Owen Jia
+ * @time: 2019/2/13 17:42
+ */
+public class JavaScriptTest {
+
+
+    public static void main(String[] args){
+        PrintUtil.printTest("This is JavaScriptTest test sample.");
+    }
+}
